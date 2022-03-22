79fa84ed5f005f335d79fca2ae50b6ae39b500c7
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-03-22 16:28:12 -0300

    Config o Admin, criei superuser e inseri alguns dados de teste no proprio admin

--- estoque/produtos/models.py
+++ estoque/produtos/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 
+
 class Produtos(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.CharField(max_length=400)
     quantidade = models.IntegerField()
     data_cadastro = models.DateTimeField('date')
+    
+    def __str__(self):
+        return self.nome
