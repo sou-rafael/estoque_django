83d3f3e3a79d518410ffffc19255757e48c8e4ed
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-03-26 14:33:52 -0300

    Ativei a listas.html, falta visualizar indivualmente na pagina visualizar.html

--- estoque/produtos/views.py
+++ estoque/produtos/views.py
@@ -1,10 +1,18 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Produtos
 
 def index(request):
     template_name = "produtos/base.html"
     return render(request,template_name)
+
+def listar(request):
+    produtos = Produtos.objects.all()
+    return render(request,"produtos/listar.html", {'produtos': produtos})
+
+def visualizar(request):
+    produto = get_object_or_404(Produtos, pk=id)
+    return render(request,"produtos/visualizar.html", {'produto': produto})
 
 def cadastrar(request):
     template_name = "produtos/cadastrar.html"
@@ -12,14 +20,6 @@ def cadastrar(request):
 
 def deletar(request):
     template_name = "produtos/deletar.html"
-    return render(request,template_name)
-
-def listar(request):
-    template_name = "produtos/listar.html"
-    return render(request,template_name)
-
-def visualizar(request):
-    template_name = "produtos/visualizar.html"
     return render(request,template_name)
 
 def atualizar(request):
