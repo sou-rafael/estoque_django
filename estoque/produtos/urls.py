83273c6686ec7726b7682604dcb1e3637a34ef4a
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-03-26 16:21:55 -0300

    Implementei as visualizaçoes, prox será cadastrar

--- estoque/produtos/urls.py
+++ estoque/produtos/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path ('', views.index, name='index'),
-    path('cadastrar', views.cadastrar, name='cadastrar'),
+    path('cadastrar/', views.cadastrar, name='cadastrar'),
     path('deletar', views.deletar, name='deletar'),
     path('listar', views.listar, name='listar'),
-    path('visualizar', views.visualizar, name='visualizar'),
+    path('<int:id>', views.visualizar, name='visualizar'),
     path('atualizar', views.atualizar, name='atualizar'),
 ]
--- estoque/produtos/views.py
+++ estoque/produtos/views.py
@@ -1,10 +1,5 @@
 from http.client import HTTPResponse
-<<<<<<< HEAD
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-=======
 from django.shortcuts import render, get_object_or_404
->>>>>>> 83d3f3e3a79d518410ffffc19255757e48c8e4ed
 from .models import Produtos
 
 def index(request):
@@ -15,7 +10,7 @@ def listar(request):
     produtos = Produtos.objects.all()
     return render(request,"produtos/listar.html", {'produtos': produtos})
 
-def visualizar(request):
+def visualizar(request, id):
     produto = get_object_or_404(Produtos, pk=id)
     return render(request,"produtos/visualizar.html", {'produto': produto})
 
@@ -27,19 +22,6 @@ def deletar(request):
     template_name = "produtos/deletar.html"
     return render(request,template_name)
 
-<<<<<<< HEAD
-def listar(request):
-    template_name = "produtos/listar.html"
-    return render(request,template_name)
-
-# Criando a funcao visualizar
-def visualizar(request):
-    template_name = "produtos/visualizar.html"
-    produtos = Produtos.objects.all()
-    return render(request,template_name, {'produtos': produtos})
-
-=======
->>>>>>> 83d3f3e3a79d518410ffffc19255757e48c8e4ed
 def atualizar(request):
     template_name = "produtos/atualizar.html"
     return render(request,template_name)
